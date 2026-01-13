62a9ab9860bd81d1f3256efbae4f0d8059b1f5dc
Nattawut.c <blackpuppy.dev@gmail.com>
2026-01-13 15:12:15 +0700

    test deploy

--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,7 +1,26 @@
-exports.getTest = (req, res) => {
+const profile = require("../models/profile");
+require("dotenv").config();
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+
+exports.getTest = async (req, res) => {
+//   try {
+//     res.status(200).json({ message: "API Working Fine" });
+//   } catch (error) {
+//     res.status(500).json({ Message: "API Error" });
+//   }
+
   try {
-    res.status(200).json({ message: "API Working Fine" });
-  } catch (error) {
-    res.status(500).json({ Message: "API Error" });
-  }
+      
+      const user_id = "DOC001"; 
+      const profileData = await profile.findOne({ user_id });
+  
+      if (!profileData) {
+        return res.status(404).json({ message: "Profile not found." });
+      }
+  
+      res.status(200).json(profileData);
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+      res.status(500).json({ message: "Error fetching profile" });
+    }
 };
